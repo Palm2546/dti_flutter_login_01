f2e9bddbf4da44262091172927318937dabac150
Palm2546 <booxpongkung@gmail.com>
2023-08-23 16:44:32 +0700

    code v.1:create project and screen file..

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:dti_flutter_login_01/views/login_a_ui.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: LoginAUI(),
+  ),
+  );
+}
